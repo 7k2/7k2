d1ccd774b643a9511250d11e013c32a1430cda97
toaster <toaster@hush.com>
2014-01-05 10:52:31 -0500

    Add player_desc.{cpp,h} from 7kaa.

---
+++ include/player_desc.h
@@ -0,0 +1,50 @@
+/*
+ * Seven Kingdoms 2: The Fryhtan War
+ *
+ * Copyright 1999 Enlight Software Ltd.
+ * Copyright 2010,2011 Jesse Allen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Filename    : player_desc.h
+// Description : MultiPlayer's PlayerDesc class
+
+#ifndef __PLAYER_DESC_H
+#define __PLAYER_DESC_H
+
+#include <mptypes.h>
+#include <enet/enet.h>
+
+class PlayerDesc
+{
+public:
+	uint32_t id;
+	char name[MP_FRIENDLY_NAME_LEN+1];
+	char connecting;
+	char authorized;
+	ENetAddress address;
+
+	PlayerDesc();
+	PlayerDesc(uint32_t id, ENetAddress *address);
+	PlayerDesc(uint32_t id, char *name);
+	PlayerDesc(char *name);
+
+	uint32_t pid();
+	char *friendly_name_str();
+	ENetAddress *get_address();
+};
+
+#endif // __PLAYER_DESC_H
---
+++ src/player_desc.cpp
@@ -0,0 +1,85 @@
+/*
+ * Seven Kingdoms 2: The Fryhtan War
+ *
+ * Copyright 1999 Enlight Software Ltd.
+ * Copyright 2010,2011 Jesse Allen
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+// Filename    : player_desc.cpp
+// Description : PlayerDesc tracks the connection of human opponents during
+//               multiplayer games.
+
+#include <player_desc.h>
+#include <string.h>
+
+PlayerDesc::PlayerDesc()
+{
+	id = 0;
+	strcpy(name, "?Anonymous?");
+	address.host = ENET_HOST_ANY;
+	address.port = 0;
+	connecting = 1;
+	authorized = 0;
+}
+
+PlayerDesc::PlayerDesc(uint32_t id, ENetAddress *address)
+{
+	this->id = id;
+	strcpy(name, "?Anonymous?");
+	this->address.host = address->host;
+	this->address.port = address->port;
+	connecting = 1;
+	authorized = 0;
+}
+
+PlayerDesc::PlayerDesc(uint32_t id, char *name)
+{
+	this->id = id;
+	strcpy(this->name, name);
+	address.host = ENET_HOST_ANY;
+	address.port = 0;
+	connecting = 1;
+	authorized = 0;
+}
+
+PlayerDesc::PlayerDesc(char *name)
+{
+	id = 0;
+	strcpy(this->name, name);
+	address.host = ENET_HOST_ANY;
+	address.port = 0;
+	connecting = 1;
+	authorized = 0;
+}
+
+uint32_t PlayerDesc::pid()
+{
+	return id;
+}
+
+char *PlayerDesc::friendly_name_str()
+{
+	return name;
+}
+
+ENetAddress *PlayerDesc::get_address()
+{
+	if (address.host == ENET_HOST_ANY)
+		return NULL;
+
+	return &this->address;
+}
